2d0aabbb51a09744ccdb3efa81aba82e5c34602e
AMPUMUZAAIJUKA <126773657+AMPUMUZAAIJUKA@users.noreply.github.com>
2024-06-07 11:50:42 +0300

    Add files via upload

---
+++ ampumuza_aijuka_Day01.py
@@ -0,0 +1,27 @@
+# Morning Session
+
+print('My name is Aijuka')
+
+r = 6
+s = 8
+sum = r+s
+print(sum)
+
+# This is a single line comment in python
+
+name = input("Your name: ")
+color = input("Enter your favorite color: ")
+print(name + " likes " + color)
+# another type of concatenation with ,
+print(name,"likes",color)
+
+''' This is 
+a multiple line
+comment'''
+
+x = 5
+y = 10
+
+print(type(x))
+
+print('The value of x is {} and y is {}'.format(x,y))
